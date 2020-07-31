3e8d7fbac065ce11c25f5e5a88b40a2e6444f6f6
Dominik <info@dominikdeimel.com>
2020-07-31 21:56:18 +0200

    removed unused css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import 'argon-design-system-react/src/assets/scss/argon-design-system-react.scss'
-import 'argon-design-system-react/src/assets/vendor/font-awesome/css/font-awesome.min.css'
-import 'argon-design-system-react/src/assets/vendor/nucleo/css/nucleo.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
