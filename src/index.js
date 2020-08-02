49e8ea278f2767c289516caf0905d5ae6bd49933
Dominik <info@dominikdeimel.com>
2020-08-02 21:27:33 +0200

    changed html lag to de, added sw

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import 'argon-design-system-react/src/assets/scss/argon-design-system-react.scss
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import * as serviceWorker from './sw';
 
 ReactDOM.render(<React.StrictMode>
   <App />
 </React.StrictMode>, document.getElementById('root'))
+
+serviceWorker.register();
